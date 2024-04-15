7a1dac4cdb6048328491d3c367d4ff47ad51bc3d
jimmywang1230 <jimmywang96172@gmail.com>
2024-04-15 17:01:22 +0800

    modify lab2-2

--- lab2/lab2-2 matrix_multiplication.s
+++ lab2/lab2-2 matrix_multiplication.s
@@ -1,60 +1,68 @@
-    AREA Matrix, CODE, READONLY ; mark first instruction
-    ENTRY
+	AREA Matrix, CODE, READONLY
+	ENTRY
 start
-    LDR R1, =MatrixA      ; Load address of MATRIXA
-    LDR R2, =MatrixB      ; Load address of MATRIXB
-    LDR R3, =MatrixC      ; Load address of MATRIXC
+    LDR R0, =MatrixA    ; Address of MATRIXA (2x3)
+    LDR R1, =MatrixB    ; Address of MATRIXB (3x2)
+    LDR R2, =MatrixC    ; Address for results MATRIXC (2x2)
 
-    ; Initialize loop indices
-    MOV R6, #0            ; i index for outer loop (MATRIXA rows)
-outer_loop
-    MOV R7, #0            ; j index for inner loop (MATRIXB columns)
-inner_loop
-    MOV R8, #0            ; k index for sum (MATRIXA columns/MATRIXB rows)
-    MOV R9, #0            ; Sum register for MATRIXC[R6, R7]
+    ; First element of MATRIXC (C[0][0] = A[0][0]*B[0][0] + A[0][1]*B[1][0] + A[0][2]*B[2][0])
+    LDR R3, [R0], #4    ; R3 = A[0][0]
+    LDR R4, [R1], #4    ; R4 = B[0][0]
+    MUL R9, R3, R4      ; R9 = A[0][0]*B[0][0]
+    LDR R3, [R0], #4    ; R3 = A[0][1]
+    LDR R4, [R1, #4]    ; R4 = B[1][0]
+    MLA R9, R3, R4, R9  ; R9 += A[0][1]*B[1][0]
+    LDR R3, [R0], #4    ; R3 = A[0][2]
+    LDR R4, [R1, #12]    ; R4 = B[2][0]
+    MLA R9, R3, R4, R9  ; R9 += A[0][2]*B[2][0]
+    STR R9, [R2]        ; Store result in MATRIXC[0][0]
 
-sum_loop
-    ; Compute effective addresses for A[i, k] and B[k, j]
-    ADD R4, R1, R6, LSL #3    ; Offset for ith row of MatrixA (i * 4 * 2)
-    ADD R4, R4, R8, LSL #2    ; Offset for kth column of MatrixA (k * 4)
-    ADD R5, R2, R8, LSL #3    ; Offset for kth row of MatrixB (k * 4 * 2)
-    ADD R5, R5, R7, LSL #2    ; Offset for jth column of MatrixB (j * 4)
+    ; Second element of MATRIXC (C[0][1] = A[0][0]*B[0][1] + A[0][1]*B[1][1] + A[0][2]*B[2][1])
+    LDR R0, =MatrixA    ; Reset R0 to the start of MATRIXA
+    LDR R3, [R0], #4    ; R3 = A[0][0]
+    LDR R4, [R1]   ; R4 = B[0][1]
+    MUL R10, R3, R4     ; R10 = A[0][0]*B[0][1]
+    LDR R3, [R0], #4    ; R3 = A[0][1]
+    LDR R4, [R1, #8]   ; R4 = B[1][1]
+    MLA R10, R3, R4, R10; R10 += A[0][1]*B[1][1]
+    LDR R3, [R0], #4    ; R3 = A[0][2]
+    LDR R4, [R1, #16]   ; R4 = B[2][1]
+    MLA R10, R3, R4, R10; R10 += A[0][2]*B[2][1]
+    STR R10, [R2, #4]   ; Store result in MATRIXC[0][1]
 
-    ; Load values and multiply
-    LDR R10, [R4]         ; Load A[i, k]
-    LDR R11, [R5]         ; Load B[k, j]
-    MUL R12, R10, R11     ; Multiply A[i, k] * B[k, j]
-    ADD R9, R9, R12       ; Accumulate result in R9
+    ; Third element of MATRIXC (C[1][0] = A[1][0]*B[0][0] + A[1][1]*B[1][0] + A[1][2]*B[2][0])
+    LDR R0, =MatrixA+12 ; Point R0 to the second row of MATRIXA
+    LDR R3, [R0], #4    ; R3 = A[1][0]
+    LDR R1, =MatrixB    ; Reset R1 to the start of MATRIXB
+    LDR R4, [R1]        ; R4 = B[0][0]
+    MUL R11, R3, R4     ; R11 = A[1][0]*B[0][0]
+    LDR R3, [R0], #4    ; R3 = A[1][1]
+    LDR R4, [R1, #8]    ; R4 = B[1][0]
+    MLA R11, R3, R4, R11; R11 += A[1][1]*B[1][0]
+    LDR R3, [R0], #4    ; R3 = A[1][2]
+    LDR R4, [R1, #16]    ; R4 = B[2][0]
+    MLA R11, R3, R4, R11; R11 += A[1][2]*B[2][0]
+    STR R11, [R2, #8]   ; Store result in MATRIXC[1][0]
 
-    ; Increment k and check if done with this sum
-    ADD R8, R8, #1
-    CMP R8, #3            ; Compare k with 3
-    BLT sum_loop          ; If k < 3, continue sum_loop
-
-    ; Store result in MATRIXC[i, j]
-    ADD R4, R3, R6, LSL #3    ; Offset for ith row of MatrixC (i * 4 * 2)
-    ADD R4, R4, R7, LSL #2    ; Offset for jth column of MatrixC (j * 4)
-    STR R9, [R4]          ; Store the computed sum
-
-    ; Increment j and check if done with this row
-    ADD R7, R7, #1
-    CMP R7, #2            ; Compare j with 2
-    BLT inner_loop        ; If j < 2, continue inner_loop
-
-    ; Increment i and check if done with all rows
-    ADD R6, R6, #1
-    CMP R6, #2            ; Compare i with 2
-    BLT outer_loop        ; If i < 2, continue outer_loop
+    ; Fourth element of MATRIXC (C[1][1] = A[1][0]*B[0][1] + A[1][1]*B[1][1] + A[1][2]*B[2][1])
+    LDR R0, =MatrixA+12 ; Point R0 to the second row of MATRIXA
+    LDR R3, [R0], #4    ; R3 = A[1][0]
+    LDR R1, =MatrixB    ; Reset R0 to the start of MATRIX
+    LDR R4, [R1, #4]   ; R4 = B[0][1]
+    MUL R12, R3, R4     ; R12 = A[1][0]*B[0][1]
+    LDR R3, [R0], #4    ; R3 = A[1][1]
+    LDR R4, [R1, #12]   ; R4 = B[1][1]
+    MLA R12, R3, R4, R12; R12 += A[1][1]*B[1][1]
+    LDR R3, [R0], #4    ; R3 = A[1][2]
+    LDR R4, [R1, #20]   ; R4 = B[2][1]
+    MLA R12, R3, R4, R12; R12 += A[1][2]*B[2][1]
+    STR R12, [R2, #12]  ; Store result in MATRIXC[1][1]
 
 stop
-    B stop               ; Infinite loop to stop the program
+    B stop              ; Infinite loop to stop the program
 
-    AREA Data, DATA, READWRITE
-MatrixA dcd 1,2,3
-        dcd 4,5,6
-MatrixB dcd 1,4
-        dcd 2,5
-        dcd 3,6
-MatrixC dcd 0,0
-        dcd 0,0
-    END ; Mark end of file
+	AREA Data, DATA, READWRITE
+MatrixA dcd 1, 2, 3, 4, 5, 6
+MatrixB dcd 7, 8, 9, 10, 11, 12
+MatrixC dcd 0, 0, 0, 0
+	END ; Mark end of file
