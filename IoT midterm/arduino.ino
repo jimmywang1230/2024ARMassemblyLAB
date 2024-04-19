2f75d9df26a84f378f0ee615363ef105b9552b90
jimmywang1230 <jimmywang96172@gmail.com>
2024-04-19 09:25:04 +0800

    add arduino code

--- IoT midterm/arduino.ino
+++ IoT midterm/arduino.ino
@@ -1,13 +1,27 @@
+#include <Servo.h>
+#include <stdbool.h>
+#include <SoftwareSerial.h>
+
+Servo myServo;  // create a servo object
+SoftwareSerial btSerial(3, 4); // TX, RX
+
+const int sensorPin = A1;
+const int redLEDPin = 7; 
+
+int sensorValue;
+bool lightsPower;
+
+int angle; // variable to hold the angle for the servo motor
+int potVal; // variable to read the value from the analog pin
+// 以下是game的
 int JoyStick_X = 0; //x
 int JoyStick_Y = 2; //y
-// int JoyStick_Z = 10; //key
 const int LIGHT_BUTTON_PIN = 5;
 const int GAME_BUTTON_PIN = 10;
 int gameMode = 0;   // 按鈕的狀態
 int A,B,C,D;
 int game_buttonState;
 int light_buttonState;
-int gg;
 void setup()
 {
   pinMode(13,OUTPUT);
@@ -17,10 +31,35 @@ void setup()
   pinMode(JoyStick_X, INPUT);
   pinMode(JoyStick_Y, INPUT);
   Serial.begin(115200);
+    // put your setup code here, to run once:
+  myServo.attach(8); // attaches the servo on pin 9 to the servo object
+  // Serial.begin(38400); // open a serial connection to your computer
+  btSerial.begin(9600); 
+  myServo.write(90);
+
+  pinMode(redLEDPin,OUTPUT);
+
+  lightsPower = 0;
 }
 int prev=0;
 void loop()
 {
+  // 藍芽控制電燈
+  if (btSerial.available()){  //如果藍牙有傳資料過來
+    int i = btSerial.read();  //把讀到的資料丟給i
+    Serial.println(i);
+    if(i == 1){    //如果是1，亮燈
+      Serial.println("Led On");
+      digitalWrite(7,HIGH);
+      myServo.write(20);
+    }
+    if(i == 2){    //如果是2，熄燈
+      Serial.println("Led Off");
+      myServo.write(20);
+      digitalWrite(7,LOW);
+    }
+  }
+
   game_buttonState = digitalRead(GAME_BUTTON_PIN);  //讀取按鍵的狀態
   light_buttonState = digitalRead(LIGHT_BUTTON_PIN);  //讀取按鍵的狀態
   // Serial.print(light_buttonState);
@@ -65,6 +104,23 @@ void loop()
     // Serial.print(C); Serial.print(",");
     // Serial.println(D); 
     Serial.write(result); // 以二進制形式顯示
+  } else if(gameMode == 0) {
+    sensorValue = analogRead(sensorPin);
+    delay(5);
+    // Serial.print("\nsensor Values \t : ");
+    // Serial.println(sensorValue);
+
+    if(sensorValue > 500 && lightsPower==1) {
+      myServo.write(20);
+      digitalWrite(7, LOW);
+      lightsPower = 0;
+      delay(3000); 
+    } else if (sensorValue > 500 && lightsPower==0) {
+      myServo.write(180);
+      digitalWrite(7, HIGH);
+      lightsPower = 1;
+      delay(3000); 
+    } 
   }
 
   delay(100);
